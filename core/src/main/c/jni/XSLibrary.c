7661d3c79369dd1a4f33b9e364c33fab52cd8b4d
valus <marcin@stelmach.biz>
2012-06-28 22:25:00 +0200

    XSLibrary.c and header added.

---
+++ core/src/main/c/jni/XSLibrary.c
@@ -0,0 +1,349 @@
+
+#include "io_crossroads_jni_XSLibrary__.h"
+#include <xs/xs.h>
+
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_bind
+ * Signature: (JLjava/lang/String;)I
+ */
+ 
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1bind
+  (JNIEnv *env, jobject obj, jlong socket, jstring address) {
+  
+  	void* socket_a = 0;
+    const char* address_a = 0;
+    int result = 0;
+    
+    socket_a = (void*) socket;
+    address_a = (*env)->GetStringUTFChars(env, address, NULL);
+    result = xs_bind(socket_a, address_a);
+    
+    return result;
+  }
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_close
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1close
+  (JNIEnv *env, jobject obj, jlong socket) {
+  
+    void* socket_a = 0;
+    int result = 0;
+    
+    socket_a = (void*) socket;
+    result = xs_close(socket_a);
+    
+    return result;
+}
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_connect
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1connect
+  (JNIEnv *env, jobject obj, jlong socket, jstring address) {
+
+    void* socket_a = 0;
+    const char* address_a = 0;
+    int result = 0;
+    
+    socket_a = (void*) socket;
+    address_a = (*env)->GetStringUTFChars(env, address, NULL);
+    result = xs_connect(socket_a, address_a);
+
+    return result;
+}
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_errno
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1errno
+  (JNIEnv *env, jobject obj) {
+  return xs_errno();
+}
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_getsockoptInt
+ * Signature: (JII)I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1getsockoptInt
+  (JNIEnv *env, jobject obj, jlong socket, jint option, jint value) {
+  
+      return 0;
+}
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_getsockoptLong
+ * Signature: (JIJ)I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1getsockoptLong
+(JNIEnv *env, jobject obj, jlong socket, jint option, jlong value) {
+    return 0;
+}
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_getsockoptByte
+ * Signature: (JI[B)I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1getsockoptByte
+(JNIEnv *env, jobject obj, jlong socket, jint option, jbyteArray value) {
+    return 0;
+}
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_init
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1init
+  (JNIEnv *env, jobject obj) {
+  
+   void* context = 0;
+    
+    context = xs_init();
+    
+    return context;
+}
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_recv
+ * Signature: (J[BII)I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1recv
+  (JNIEnv *env, jobject obj, jlong socket, jbyteArray buffer, jint length, jint flags) {
+  
+    void* socket_a = 0;
+    jbyte* buffer_a = 0;
+    int result = 0;
+    
+    socket_a = (void*) socket;
+    buffer_a = (jbyte*) (*env)->GetDirectBufferAddress(env, buffer);
+    result = xs_recv(socket_a, buffer_a, length, flags);
+    
+    return result;
+}
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_send
+ * Signature: (J[BII)I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1send
+  (JNIEnv *env, jobject obj, jlong socket, jbyteArray buffer, jint length, jint flags) {
+  
+  	void* socket_a = 0;
+    jbyte* buffer_a = 0;
+    int result = 0;
+    
+    socket_a = (void*) socket;
+    buffer_a = (jbyte*) (*env)->GetDirectBufferAddress(env, buffer);
+    result = xs_send(socket_a, buffer_a, length, flags);
+
+    return result;
+  }
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_setctxoptInt
+ * Signature: (JII)I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1setctxoptInt
+(JNIEnv *env, jobject obj, jlong context, jint option, jint value) {
+    return 0;
+}
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_setctxoptLong
+ * Signature: (JIJ)I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1setctxoptLong
+(JNIEnv *env, jobject obj, jlong context, jint option, jlong value) {
+    return 0;
+}
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_setctxoptByte
+ * Signature: (JI[B)I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1setctxoptByte
+(JNIEnv *env, jobject obj, jlong context, jint option, jbyteArray value) {
+    return 0;
+}
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_setsockoptInt
+ * Signature: (JII)I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1setsockoptInt
+(JNIEnv *env, jobject obj, jlong socket, jint option, jint value) {
+    return 0;
+}
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_setsockoptLong
+ * Signature: (JIJ)I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1setsockoptLong
+(JNIEnv *env, jobject obj, jlong socket, jint option, jlong value) {
+    return 0;
+}
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_setsockoptByte
+ * Signature: (JI[B)I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1setsockoptByte
+(JNIEnv *env, jobject obj, jlong socket, jint option, jbyteArray value) {
+    return 0;
+}
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_shutdown
+ * Signature: (JI)I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1shutdown
+  (JNIEnv *env, jobject obj, jlong socket, jint how) {
+  
+    void* socket_a = 0;
+    int result = 0;
+    
+    socket_a = (void*) socket;
+    result = xs_shutdown(socket_a, how);
+
+    return result;
+  }
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_socket
+ * Signature: (JI)J
+ */
+JNIEXPORT jlong JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1socket
+  (JNIEnv *env, jobject obj, jlong context, jint type) {
+  
+    void* context_a = 0;
+    void* socket_a =  0;
+    
+    context_a = (void*) context;
+    socket_a = xs_socket(context_a, type);
+    
+    return socket_a;
+  }
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_strerror
+ * Signature: (I)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1strerror
+  (JNIEnv *env, jobject obj, jint errnum) {
+  
+    const char* error = 0;
+    jstring answer =  0;
+    
+    error = xs_strerror(errnum);
+    answer = (*env)->NewStringUTF(env, error);
+    return answer;
+  }
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_term
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1term
+  (JNIEnv *env, jobject obj, jlong context) {
+  
+    void* context_a = 0;
+    int result = 0;
+    
+    context_a = (void*) context;
+    result = xs_term(context_a);
+    
+    return result;
+  }
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_version
+ * Signature: ([I[I[I)V
+ */
+JNIEXPORT void JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1version
+  (JNIEnv *env, jobject obj, jintArray major, jintArray minor, jintArray patch) {
+  
+    jclass cmaj = 0;
+    jclass cmin = 0;
+    jclass cpat = 0;
+    jfieldID fmaj = 0;
+    jfieldID fmin = 0;
+    jfieldID fpat = 0;
+    int imaj = 0;
+    int imin = 0;
+    int ipat = 0;
+    
+    cmaj = (*env)->GetObjectClass(env, major);
+
+    fmaj = (*env)->GetFieldID(env, cmaj, "value", "I");
+    cmin = (*env)->GetObjectClass(env, minor);
+    fmin = (*env)->GetFieldID(env, cmin, "value", "I");
+    cpat = (*env)->GetObjectClass(env, patch);   
+    fpat = (*env)->GetFieldID(env, cpat, "value", "I");
+    xs_version(&imaj, &imin, &ipat);
+    (*env)->SetIntField(env, major, fmaj, imaj);
+    (*env)->SetIntField(env, minor, fmin, imin);
+    (*env)->SetIntField(env, patch, fpat, ipat);
+    
+    return (imaj*100+imin)*100+ipat;
+  }
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_stopwatch_start
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1stopwatch_1start
+  (JNIEnv *env, jobject obj) {
+  
+    void* watch = 0;
+    watch = xs_stopwatch_start();
+    
+    return (jlong) watch;
+  }
+
+/*
+ * Class:     io_crossroads_jni_XSLibrary__
+ * Method:    xs_stopwatch_stop
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_io_crossroads_jni_XSLibrary_00024_xs_1stopwatch_1stop
+  (JNIEnv *env, jobject obj, jlong watch) {
+  
+    void* watch_a = 0;
+    long result = 0;
+    
+    watch_a = (void*) watch;
+    result = xs_stopwatch_stop(watch_a);
+    
+    return result;
+  }
+
+#ifdef __cplusplus
+}
+#endif
